12d405aefe903dc2de92e8c05d5dc26033d2a905
Mark Piper <mark.piper@wds.co>
2015-03-20 18:25:02 +0000

    MP: Rename actor

--- app/controllers/WSControl.java
+++ app/controllers/WSControl.java
@@ -1,13 +1,12 @@
 package controllers;
 
-import models.MyWebSocketActor;
+import models.WSHandlerActor;
 import play.mvc.Controller;
 import play.mvc.WebSocket;
 
 public class WSControl extends Controller {
 	
 	public static WebSocket<String> bind() {
-		return WebSocket.withActor(MyWebSocketActor::props);
+		return WebSocket.withActor(WSHandlerActor::props);
 	}
-
 }
--- app/models/WSHandlerActor.java
+++ app/models/WSHandlerActor.java
@@ -4,20 +4,21 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 
-public class MyWebSocketActor extends UntypedActor {
+public class WSHandlerActor extends UntypedActor {
 
 	private final ActorRef out;
 
-	public MyWebSocketActor(ActorRef out) {
+	public WSHandlerActor(ActorRef out) {
 		this.out = out;
 	}
 
 	public static Props props(ActorRef out) {
-		return Props.create(MyWebSocketActor.class, out);
+		return Props.create(WSHandlerActor.class, out);
 	}
 
 	@Override
 	public void onReceive(Object message) throws Exception {
+		System.out.println(out);
 		if (message instanceof String) {
 			out.tell("I received your message: " + message, self());
 		}
