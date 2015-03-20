43a040b844bdcc8f2f94489e4e1f2ae79f31ac03
Mark Piper <mark.piper@wds.co>
2015-03-20 09:57:55 +0000

    MP: Format code

--- app/models/MyWebSocketActor.java
+++ app/models/MyWebSocketActor.java
@@ -7,20 +7,19 @@ import akka.actor.UntypedActor;
 public class MyWebSocketActor extends UntypedActor {
 
 	private final ActorRef out;
-	
+
 	public MyWebSocketActor(ActorRef out) {
 		this.out = out;
 	}
-	
+
 	public static Props props(ActorRef out) {
 		return Props.create(MyWebSocketActor.class, out);
 	}
-	
+
 	@Override
 	public void onReceive(Object message) throws Exception {
-		 if (message instanceof String) {
-	            out.tell("I received your message: " + message, self());
-	        }
+		if (message instanceof String) {
+			out.tell("I received your message: " + message, self());
+		}
 	}
-
 }
