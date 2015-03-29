214a08a1a2c197f60172f7cd74c7c78eceadb111
Mark Piper <mark.piper@wds.co>
2015-03-29 22:55:22 +0100

    MP: Reconnect on socket close

--- public/javascripts/app/quiz.js
+++ public/javascripts/app/quiz.js
@@ -1,11 +1,16 @@
 require(["jquery", "bootstrap"], function ($) {
-	var socket = new WebSocket("ws://" + window.location.hostname + ":" + window.location.port + "/api/bind?teamName=" + teamName);
+	var socket = register();
 	
+	function register() {
+		var socket = new WebSocket("ws://" + window.location.hostname + ":" + window.location.port + "/api/bind?teamName=" + teamName);
+		return socket;	
+	}
 	socket.onmessage = function(event) {
 		$("#state").html(event.data)
 	}	
 	
 	socket.onclose = function(event) {
-		alert("oh, I lost connection");
+		alert("lost connection - reregistering");
+		socket = register;
 	}
 });
