c75a3a83e4544554c3ad473f04431feb1580c3fe
Mark Piper <mark.piper@wds.co>
2015-03-31 18:15:13 +0100

    MP: MVC admin

--- public/javascripts/app/admin.js
+++ public/javascripts/app/admin.js
@@ -1,22 +1,46 @@
 require(["jquery", "bootstrap"], function($){
-	var socket = new WebSocket("ws://" + window.location.hostname + ":" + window.location.port + "/api/bind?teamName=");
-
-	socket.onmessage = function(event) {
-		var obj = JSON.parse(event.data);
-		if(obj.type == 'teamList') {
+	var view = new View();
+	var controller = new Controller(view);
+	view.setController(controller);
+	
+	function Controller(view) {
+		this.socket = new WebSocket("ws://" + window.location.hostname + ":" + window.location.port + "/api/bind?teamName=");
+		this.view = view;
+		var this_ = this;
+		
+		this.socket.onmessage = function(event) {
+			var obj = JSON.parse(event.data);
+			if(obj.type == 'teamList') {
+				this_.view.displayTeamList(obj.teams)
+			}
+		}
+		
+		this.nextQuestion = function() {
+			this_.socket.send(JSON.stringify(new NextQuestion()));
+		}
+		
+	}
+	
+	function View() {
+		var this_ = this;
+		
+		this.setController = function(controller) {
+			this_.controller = controller;
+		}
+		
+		this.displayTeamList = function(teams) {
 			$("#teams").html("");
-			obj.teams.forEach(function(team) {
+			teams.forEach(function(team) {
 				$("#teams").append($("<li />").html(team));
 			})
 		}
+		
+		$("#nextQuestion").click(function() {
+			this_.controller.nextQuestion()
+		})
 	}
-	
-	$("#nextQuestion").click(function() {
-		socket.send(JSON.stringify(new NextQuestion()));
-	})
 	
 	function NextQuestion() {
 		this.type = "nextQuestion";
 	}
-	
 })
