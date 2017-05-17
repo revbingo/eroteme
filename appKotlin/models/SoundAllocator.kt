abda404299817c485a06627d252f9e8117ea4164
Mark Piper <mark.piper@wds.co>
2017-05-17 20:11:55 +0100

    Whoops, mysterious gst in middle of file name

--- appKotlin/models/SoundAllocator.kt
+++ appKotlin/models/SoundAllocator.kt
@@ -5,7 +5,7 @@ import java.util.*
 class SoundAllocator {
 
     val allSounds = mutableListOf("fart", "rooster", "ping", "bicycle_bell", "boom_x", "burp2_x",
-                                    "cuckoo_clogstck2_x", "doorbell2", "honk2_x", "aoogah")
+                                    "cuckoo_clock2_x", "doorbell2", "honk2_x", "aoogah")
 
     fun allocateSound(): String {
         if(allSounds.isEmpty()) return "ping"
